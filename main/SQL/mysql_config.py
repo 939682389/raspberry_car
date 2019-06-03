fc4905b49b1747607a9dd7ffcd1086cb413d0650
939682389 <40315426+939682389@users.noreply.github.com>
2019-06-03 12:44:23 +0800

    Update mysql_config.py

--- main/SQL/mysql_config.py
+++ main/SQL/mysql_config.py
@@ -2,7 +2,7 @@ HOST = "sh-cdb-lksdvfsw.sql.tencentcdb.com"
 PORT = "63013"
 DB = "pi"
 USER = "root"
-PASS = "zhang990113"
+PASS = ""
 CHARSET = "utf8"
 DB_URI = "mysql+pymysql://{}:{}@{}:{}/{}?charset={}".format(USER, PASS, HOST, PORT, DB, CHARSET)
 SQLALCHEMY_DATABASE_URI = DB_URI
